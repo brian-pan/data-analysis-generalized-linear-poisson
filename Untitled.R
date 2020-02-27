55d04e5a411a2354cdca89ebc6a7132e8edf0a7a
Brian Pan <brianpanzifan@gmail.com>
2020-02-26 22:31:43 -0500

    initial commit

--- Untitled.R
+++
@@ -1,3 +0,0 @@
-library(faraway)
-library(Pmisc)
-library(tidyverse)
---
+++ data-analysis-glm-poisson-fiji-practice.R
@@ -0,0 +1,38 @@
+# load data
+File = 'fijiDownload.RData'
+load(File)
+
+# explore data
+head(fijiFull)
+
+# First get rid of newly married women, and those who are not educated:
+fijiSub = fiji[fiji$monthsSinceM > 0 & !is.na(fiji$literacy), ]
+# make a offset and make it log-based:
+fijiSub$logYrsSinceM = log(fijiSub$monthsSinceM/12)
+# make 15-18 years old the reference group:
+fijiSub$ageMarried = relevel(fijiSub$ageMarried, '15to18')
+# make the woman in rural area the reference group:
+fijiSub$urban = relevel(fijiSub$residence, 'rural')
+
+# glm regression:
+fijiGlm = glm(
+  children ~ offset(logYrsSinceM) + ageMarried + urban + literacy + ethnicity,
+  family = poisson (link = log), data = fijiSub)
+
+summary(fijiGlm)
+
+# make a 95% CI:
+rate = cbind(est = fijiGlm$coefficients, confint(fijiGlm, level = 0.95))
+
+# table
+knitr::kable(cbind(summary(
+  fijiRes)$coefficients,
+  exp(rate)),
+  digits = 2) 
+
+# glm regression 2:
+fijiGlm2 = glm(
+  children ~ offset(logYrsSinceM) + ageMarried + urban + ethnicity,
+  family = poisson(link = log), data = fijiSub)
+
+summary(fijiGlm2)
